f161c5dfc6a68ad73754dd7cbe74995b0496bbce
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-09-06 17:04:03 -0300

    implementndor o Post Put e Delete da model de Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,43 @@ import Login from './paginas/login/Login';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
     <Router>
-    <Navbar />
-    <Routes>
+      <Navbar />
+      <Routes>
 
-      <Route path="/" element={<Login  />} />
+        <Route path="/" element={<Login />} />
 
-      <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
 
-      <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-      <Route path="/temas" element={<ListaTema />} />
+        <Route path="/temas" element={<ListaTema />} />
 
         <Route path="/posts" element={<ListaPostagem />} />
 
-    </Routes>
-    <Footer />
-  </Router>
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+        <Route path="/formularioTema" element={<CadastroTema />} />
+
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+      </Routes>
+      <Footer />
+    </Router>
 
   );
 }
